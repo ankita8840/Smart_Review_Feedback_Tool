aa1f645b1c7a18e8ba58d5053cfbbc9a08bf7081
ankita8840 <ankitagupta993608@gmail.com>
2026-09-18 23:20:29 +0530

    Update backend server and clean frontend README

--- backend/Server.js
+++ backend/Server.js
@@ -27,6 +27,6 @@ app.use("/api/reviews", reviewRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on PORT ${PORT}`);
 });
